901ad2af1aa9669c614334ad9618d2fb18e26d91
andrewpetrovic <andrewpetrovic@gmail.com>
2015-12-28 15:41:18 +0800

    reversion cleanup.sh

--- linux/advanced_bash_scripting/examples/cleanup.sh
+++ linux/advanced_bash_scripting/examples/cleanup.sh
@@ -1,6 +1,16 @@
-#	Cleanup version 1
-#	使用root权限运行
-cd	/var/log
+#!/bin/bash
+#	Bash脚本标准起始行。
+#	Cleanup,	version	2
+#	使用root权限运行。
+#	在这里插入代码用来打印错误信息,并且在未使用root权限时退出。
+
+LOG_DIR=/var/log
+#	变量比硬编码(hard-coded)要更合适一些
+cd	$LOG_DIR
+
 cat	/dev/null	>	messages
 cat	/dev/null	>	wtmp
-echo	"Log	files	cleaned	up."
+
+echo	"Logs	cleaned	up."
+exit	#	这是正确终止脚本的方式。
+					#	不带参数的exit返回的是上一条命令的结果。
